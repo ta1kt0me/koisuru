c39253c4fca0735f62838b3ad9fb1a87292edd31
ta1kt0me <p.wadachi@gmail.com>
2016-03-25 02:48:46 +0900

    Implement to show http response

--- client/components/main.js
+++ client/components/main.js
@@ -4,21 +4,37 @@ import ReactDOM from 'react-dom'
 export class Root extends React.Component {
   constructor(props) {
     super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
     this.state = { messages: [] }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    var input = ReactDOM.findDOMNode(this.refs.message).value
+
+    fetch('http://localhost:9393/answer')
+    .then(function(response){
+      return response.json()
+    }).then(function(json){
+      ReactDOM.findDOMNode(this.refs.message).value = ''
+      this.setState({
+        messages: this.state.messages.concat(json.name + ':' + json.responder + '> ' + json.answer)
+      })
+    }.bind(this))
   }
 
   render() {
     return (
       <div>
         <h1>Message</h1>
-        <form className="messageForm">
+        <form className="messageForm" onSubmit={this.handleSubmit}>
           <input type="text" placeholder="message..." ref="message" />
           <input type="submit" value="Post" />
         </form>
         <ul>
         {
           this.state.messages.map(function(message) {
-            return <li key={message}>{message}</li>
+            return <li>{message}</li>
           })
         }
         </ul>
